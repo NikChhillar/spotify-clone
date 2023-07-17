37afdb5e0f6824ecc9f92423bc93aaae6e6727eb
Nikhil <nikhilchhillar29@gmail.com>
2023-07-17 21:21:02 +0530

    initial commit

---
+++ app/(site)/page.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/no-unescaped-entities */
+import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
+
+export default function Home() {
+  return (
+    <div className="bg-neutral-900 rounded-lg h-full overflow-hidden overflow-y-auto">
+      <Header>
+        <div className="mb-2">
+          <h1 className="text-white text-3xl font-semibold">Welcome back</h1>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+            <ListItem
+              image="/images/liked.png"
+              name="Ruby's Songs"
+              href="liked"
+            />
+          </div>
+        </div>
+      </Header>
+      <div className="px-6 mb-7 mt-2">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">Ai's Songs</h1>
+        </div>
+        Lists of songs....
+      </div>
+    </div>
+  );
+}
