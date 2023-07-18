92ea0218d439a186b292fe65c4c9abb339a3859e
Nikhil <nikhilchhillar29@gmail.com>
2023-07-18 22:12:26 +0530

    fetched user songs

--- components/Library.tsx
+++ components/Library.tsx
@@ -5,10 +5,14 @@ import { AiOutlinePlus } from "react-icons/ai";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
+import { Song } from "@/types";
+import MediaItem from "./MediaItem";
 
-interface LibraryProps {}
+interface LibraryProps {
+  songs: Song[];
+}
 
-const Library = () => {
+const Library: React.FC<LibraryProps> = ({ songs }) => {
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
   const { user } = useUser();
@@ -34,7 +38,11 @@ const Library = () => {
           className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
       </div>
-      <div className="flex flex-col gap-y-2 mt-4 px-3">List here....</div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">
+        {songs.map((song) => (
+          <MediaItem onClick={(id: string) => {}} key={song.id} data={song} />
+        ))}
+      </div>
     </div>
   );
 };
