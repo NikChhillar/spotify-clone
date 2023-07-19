71cb9662e8b056eb90d0829c6eb9a5cbcea6e1ea
Nikhil <nikhilchhillar29@gmail.com>
2023-07-19 14:51:17 +0530

    added player

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import SongItem from "@/components/SongItem";
+import useOnPlay from "@/hooks/useOnPlay";
 import { Song } from "@/types";
 import React from "react";
 
@@ -11,6 +12,8 @@ interface PageContentProps {
 const PageContent: React.FC<PageContentProps> = ({ songs }) => {
   //
 
+  const onPlay = useOnPlay(songs);
+
   if (songs.length === 0) {
     return <div className="mt-4 text-neutral-400">No songs available....</div>;
   }
@@ -18,7 +21,11 @@ const PageContent: React.FC<PageContentProps> = ({ songs }) => {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-4 mt-4">
       {songs.map((song) => (
-        <SongItem key={song.id} onClick={() => {}} data={song} />
+        <SongItem
+          key={song.id}
+          onClick={(id: string) => onPlay(id)}
+          data={song}
+        />
       ))}
     </div>
   );
